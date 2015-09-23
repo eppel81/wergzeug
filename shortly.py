baae058a0fd572c4972d1ba80e035e3800ebdd67
dmitry.gogenko <gogenko@ukr.net>
2015-09-23 11:08:18 +0300

    поменял порт на 5000 и добавил комментарии

--- shortly.py
+++ shortly.py
@@ -11,6 +11,9 @@ from jinja2 import Environment, FileSystemLoader
 
 
 class shortly(object):
+    """
+    Класс, создающий основное приложение
+    """
     def __init__(self, config):
         self.redis = redis.Redis(config['redis_host'], config['redis_port'])
 
@@ -23,14 +26,20 @@ class shortly(object):
             Rule('/<short_id>+', endpoint='short_link_details')
         ])
 
-        # import pdb
-        # pdb.set_trace()
-
     def render_template(self, template_name, **context):
+        """
+        метод загружает определенный шаблон, связывает его с контекстом и возвращает объект Response(готовая страница)
+        """
         t = self.jinja_env.get_template(template_name)
         return Response(t.render(context), mimetype='text/html')
 
     def on_new_url(self, request):
+        """
+        Метод загружает стартовую страницу приложения. Вызывается при запросе корня сайта '/',
+        а также при передаче данных формы для сохранения ссылок.
+
+        После успешного сохранения ссылки выполняется редирект на страницу с подробным описанием добавленной ссылки.
+        """
         error = None
         url = ''
         # import pdb
@@ -50,7 +59,8 @@ class shortly(object):
 
     def get_all_shortlies(self):
         """
-        Метод для получения всех сохраненных ссылок. Возвращает список словарей каждой ссылки
+        Метод для получения всех сохраненных ссылок в redis. Возвращает список словарей для каждой ссылки.
+        Все ссылки отображаются только на стартовой странице.
         """
         links_list = []
         num_saved_links = int(self.redis.get('last_url_id') or 0) + 1
@@ -60,13 +70,11 @@ class shortly(object):
             click_count = int(self.redis.get('click-count:' + short_id) or 0)
             links_list.append(dict(short_id = short_id, url_target = link_target, click_count = click_count))
 
-        # import pdb
-        # pdb.set_trace()
         return links_list
 
     def on_follow_short_link(self, request, short_id):
         """
-        Вызывается при переходе на shortly-ссылку - переходим на полную ссылку
+        Вызывается при клике на shortly-ссылке. Как результат - переходим на полную ссылку
         """
         link_target = self.redis.get('url_target:' + short_id)
         if link_target is None:
@@ -75,6 +83,9 @@ class shortly(object):
         return redirect(link_target)
 
     def on_short_link_details(self, request, short_id):
+        """
+        Метод загружает страницу с детальным описанием сохраненной короткой ссылки (сколько переходов...)
+        """
         link_target = self.redis.get('url_target:' + short_id)
         if link_target is None:
             raise NotFound()
@@ -84,7 +95,8 @@ class shortly(object):
 
     def dispatch_request(self, request):
         """
-        Метод Диспетчер
+        Метод определяет какой self-метод вызывать в зависимости от запроса. Если нет вызываемого метода,
+        тогда возвращает wsgi-exception.
         """
         adapter = self.url_map.bind_to_environ(request.environ)
         try:
@@ -96,11 +108,17 @@ class shortly(object):
             return e
 
     def wsgi_app(self, environ, start_response):
+        """
+        Это wsgi-приложение)
+        """
         request = Request(environ)
         response = self.dispatch_request(request)
         return response(environ, start_response)
 
     def insert_url(self, url):
+        """
+        Метод для записи ключей ссылки в redis
+        """
         short_id = self.redis.get('reverse-url:' + url)
         if short_id is not None:
             return short_id
@@ -116,13 +134,16 @@ class shortly(object):
 
 def is_valid_url(url):
     """
-    Валидация по схеме в URL
+    Валидация по cheme в URL
     """
     parts = urlparse.urlparse(url)
     return parts.scheme in ('http', 'https')
 
 
 def base36_encode(number):
+    """
+    перевод в 36-ричную систему счисления
+    """
     assert number >= 0, 'positive integer required'
     if number == 0:
         return 0
@@ -134,6 +155,9 @@ def base36_encode(number):
 
 
 def create_app(redis_host='localhost', redis_port=6379, with_static=True):
+    """
+    Функция создает приложение с настройками redis и оборачивает self.wsgi_app для доступа к статичным файлам
+    """
     app = shortly({
         'redis_host': redis_host,
         'redis_port': redis_port
@@ -148,4 +172,4 @@ if __name__ == '__main__':
     from werkzeug.serving import run_simple
 
     app = create_app()
-    run_simple('127.0.0.1', 5001, app, use_debugger=True, use_reloader=True)
+    run_simple('127.0.0.1', 5000, app, use_debugger=True, use_reloader=True)
